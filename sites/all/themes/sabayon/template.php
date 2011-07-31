09c9ef3d7a46006463442f1c5d2aaa53f63bc251
Ian Whyman <ian.whyman@sabayon.org>
2011-07-31 18:10:01 +0200

    [themes/sabayon] Remove colons after field labels

--- sites/all/themes/sabayon/template.php
+++ sites/all/themes/sabayon/template.php
@@ -206,4 +206,32 @@ function sabayon_item_list($variables) {
 	$output .= '</div>';
 	return $output;
 }
+
+/*
+ *  Remove colons from field labels
+ */
+
+function sabayon_field($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . '</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
+
 ?>
+
